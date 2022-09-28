2fb994f00687672b24e6a443f508acec8b076a00
Pierre-Louis <plauger@keley.com>
2022-09-28 12:45:34 +0200

    NEw version

---
+++ AXAMAP2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Background" tilewidth="32" tileheight="32" tilecount="1408" columns="44">
+ <image source="tilesets/Background.jpg" width="1408" height="1025"/>
+</tileset>
